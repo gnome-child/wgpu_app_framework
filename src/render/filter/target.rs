ca27eab409586aab5f865eada92234db872ad467
gnome-child <sheathompson1995@proton.me>
2026-07-09 03:04:47 -0500

    Tighten filter target visibility

--- src/render/filter/target.rs
+++ src/render/filter/target.rs
@@ -8,7 +8,7 @@ pub struct Target {
 }
 
 impl Target {
-    pub fn new(canvas: &render::Canvas) -> Self {
+    pub(crate) fn new(canvas: &render::Canvas) -> Self {
         Self {
             physical_area: canvas.physical_area(),
             logical_area: canvas.logical_area(),
@@ -16,7 +16,7 @@ impl Target {
         }
     }
 
-    pub fn from_viewport(viewport: render::Viewport) -> Self {
+    pub(crate) fn from_viewport(viewport: render::Viewport) -> Self {
         Self {
             physical_area: viewport.physical_area(),
             logical_area: viewport.logical_area(),
@@ -24,7 +24,7 @@ impl Target {
         }
     }
 
-    pub fn from_logical_area(logical_area: paint::area::Logical, scale_factor: f32) -> Self {
+    pub(crate) fn from_logical_area(logical_area: paint::area::Logical, scale_factor: f32) -> Self {
         Self {
             physical_area: logical_area.to_physical(scale_factor).clamp_min(1),
             logical_area,
@@ -32,11 +32,11 @@ impl Target {
         }
     }
 
-    pub fn physical_area(self) -> paint::area::Physical {
+    pub(crate) fn physical_area(self) -> paint::area::Physical {
         self.physical_area
     }
 
-    pub fn logical_area(self) -> paint::area::Logical {
+    pub(crate) fn logical_area(self) -> paint::area::Logical {
         self.logical_area
     }
 }
